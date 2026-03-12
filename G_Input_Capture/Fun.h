1dcc342e77d075956360770b2183c807bcb3c579
CHUANG <3418732870@qq.com>
2026-03-12 10:57:36 +0800

    输入捕获PWM波

---
+++ G_Input_Capture/Fun.h
@@ -0,0 +1,29 @@
+#ifndef _Fun_h
+#define _Fun_h
+
+#include "Head.h"
+void LED_Show(uint8_t LED,uint8_t mode);
+void Key_Scan(void);
+void LCD_Show(void);
+
+//全局变量
+extern uint8_t  Key_B1;
+extern uint8_t  Key_B1_Last ;
+extern uint8_t  Key_B2;
+extern uint8_t  Key_B2_Last ;
+extern uint8_t  Key_B3;
+extern uint8_t  Key_B3_Last ;
+
+extern char     string[20];
+extern int      counter;
+extern uint8_t  LED_Mode;
+extern uint8_t  High_Line ;
+extern uint32_t capture_value1 ,capture_value2;
+extern uint32_t fre1,fre2;
+//宏定义
+#define Sys_fre (80000000) //系统时钟
+#define Psc     (80-1)     //预分频
+#define High_Show_Num (5) //高亮显示行数
+
+//
+#endif
---
+++ G_Input_Capture/Head.h
@@ -0,0 +1,16 @@
+#ifndef _Head_h
+#define _Head_h
+
+#include "RTE_Components.h"             // Component selection
+
+#include "main.h"
+#include "gpio.h"
+#include "string.h"
+#include "stdio.h"
+#include "stdint.h"
+
+#include "Fun.h"
+#include "lcd.h"
+#include "tim.h"
+
+#endif

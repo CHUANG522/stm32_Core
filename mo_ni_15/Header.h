09642bbc2d4aa16178c9c03bec630c160f4f2106
CHUANG <3418732870@qq.com>
2026-03-14 21:32:05 +0800

    第十五届模拟题(第一次自己写这么多代码，✌）
    核心
    只增一次
    led利用逻辑亮灭
    获得某段时间
    标志位及last标志位的灵活使用
    按键长按逻辑
    初始化的赋值
    考虑形除法是否用到小数
    数组大小设大不设小
    定时器获得时间
    位运算

---
+++ mo_ni_15/Header.h
@@ -0,0 +1,19 @@
+#ifndef  _HEADER_H
+#define  _HEADER_H
+
+#include "RTE_Components.h"             // Component selection
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <lcd.h>
+
+#include "main.h"
+#include "gpio.h"
+#include "fun.h"
+
+
+
+#endif
+
+

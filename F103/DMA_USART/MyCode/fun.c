793f0082446f6833d9d94e4d860480f5f1ee681e
CHUANG <3418732870@qq.com>
2026-06-25 17:51:49 +0800

    feat(DMA_USART):一种更简便的1秒更新写法
    只使用time_OLED一个变量
    在他大于1000时直接清零即可

--- F103/DMA_USART/MyCode/fun.c
+++ F103/DMA_USART/MyCode/fun.c
@@ -8,10 +8,9 @@ uint32_t time_OLED;
 uint32_t time;
 void OLED_show(void)
 {
-	static uint32_t last_time_OLED;
-	if (time_OLED - last_time_OLED < 1000)
+	if (time_OLED < 1000)
 		return; // 1秒刷新
-	last_time_OLED = time_OLED;
+	time_OLED = 0;
 	send_string((uint8_t *)"hello\r\n");
 	OLED_Clear();
 	sprintf((char *)OLED_buff, "time_OLED:%d", time_OLED);
